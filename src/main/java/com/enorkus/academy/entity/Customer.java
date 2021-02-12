cc7252a9ed75cc113463a35d0326328e544301b6
Nikolaj Grigorjev <grigorjev.nikolaj@gmail.com>
2021-02-12 09:04:59 +0200

    commented code removed

--- src/main/java/com/enorkus/academy/entity/Customer.java
+++ src/main/java/com/enorkus/academy/entity/Customer.java
@@ -102,7 +102,6 @@ public class Customer {
         }
     }
 
-
     public String getId() {
         return id;
     }
@@ -111,89 +110,45 @@ public class Customer {
         return firstName;
     }
 
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-
     public String getLastName() {
         return lastName;
     }
-
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
 
     public String getPersonalNumber() {
         return personalNumber;
     }
 
-//    public void setPersonalNumber(String personalNumber) {
-//        this.personalNumber = personalNumber;
-//    }
-
     public String getMiddleName() {
         return middleName;
     }
-
-//    public void setMiddleName(String middleName) {
-//        this.middleName = middleName;
-//    }
 
     public int getAge() {
         return age;
     }
 
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-
     public String getCountryCode() {
         return countryCode;
     }
-
-//    public void setCountryCode(String countryCode) {
-//        this.countryCode = countryCode;
-//    }
 
     public String getCity() {
         return city;
     }
 
-//    public void setCity(String city) {
-//        this.city = city;
-//    }
-
     public int getMonthlyIncome() {
         return monthlyIncome;
     }
-
-//    public void setMonthlyIncome(int monthlyIncome) {
-//        this.monthlyIncome = monthlyIncome;
-//    }
 
     public String getGender() {
         return gender;
     }
 
-//    public void setGender(String gender) {
-//        this.gender = gender;
-//    }
-
     public String getEmployer() {
         return employer;
     }
 
-//    public void setEmployer(String employer) {
-//        this.employer = employer;
-//    }
-
     public String getMaritalStatus() {
         return maritalStatus;
     }
-
-//    public void setMaritalStatus(String maritalStatus) {
-//        this.maritalStatus = maritalStatus;
-//    }
 
     public void setId(String id) {
         this.id = id;
